5d55f8eee9fc49e09b7e7a5abafb1155f50777df
Alexandr Horbachev <asggmark@gmail.com>
2019-10-28 00:15:12 +0200

    added propTypes check

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const filmsNames = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Inception`];
+const filmsGenres = [`All genres`, `Comedies`, `Crime`, `Documentary`, `Dramas`, `Horror`, `Kids & Family`, `Romance`, `Sci-Fi`, `Thrillers`];
+
 const init = () => {
   ReactDOM.render(
-      <App/>,
+      <App
+        films={filmsNames}
+        genres={filmsGenres}
+      />,
       document.querySelector(`#root`)
   );
 };
